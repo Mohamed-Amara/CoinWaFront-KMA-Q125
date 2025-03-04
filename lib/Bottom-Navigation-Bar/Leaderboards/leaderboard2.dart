689fbaf9251802348d5db8b7e4576fa93c5defd6
arian <70283699+arian-gif@users.noreply.github.com>
2025-03-04 12:46:17 -0500

    Update leaderboard2.dart

--- lib/Bottom-Navigation-Bar/Leaderboards/leaderboard2.dart
+++ lib/Bottom-Navigation-Bar/Leaderboards/leaderboard2.dart
@@ -1,5 +1,3 @@
-//Errors RN: not working: coin values on podium or list, rank values
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Providers/coin_provider.dart';
 import 'package:flutter_application_1/Providers/leader_provider.dart';
